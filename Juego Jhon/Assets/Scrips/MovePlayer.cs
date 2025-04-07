1d73af41bf2f05e5fbe9d9a5a9d118079f8af39c
valentinamonterozapata <valentina.montero@uao.edu.co>
2025-04-07 16:13:06 -0500

    Acomode la escena 3 (ambientación) y las vidas del player

--- Juego Jhon/Assets/Scrips/MovePlayer.cs
+++ Juego Jhon/Assets/Scrips/MovePlayer.cs
@@ -14,7 +14,7 @@ public class MoverPlayer : MonoBehaviour
     public float checkRadius = 0.5f;
     public LayerMask whatIsGround;
     private float LastShoot;
-    private int Health = 5; 
+    private int Health = 10; 
 
     void Start()
     {
